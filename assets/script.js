faa0fa27c27758265aba5c1ee255d8f4a46ee9d9
Hillary Mason <hillary.mason@outlook.com>
2023-11-20 13:30:52 +0000

    added function to create array with chosen characters

--- assets/script.js
+++ assets/script.js
@@ -1,99 +1,3 @@
-var Characters = {
-  specialCharacters: [
-    '@',
-    '%',
-    '+',
-    '\\',
-    '/',
-    "'",
-    '!',
-    '#',
-    '$',
-    '^',
-    '?',
-    ':',
-    ',',
-    ')',
-    '(',
-    '}',
-    '{',
-    ']',
-    '[',
-    '~',
-    '-',
-    '_',
-    '.'
-  ],
-  numericCharacters: [
-    '0', 
-    '1', 
-    '2', 
-    '3', 
-    '4', 
-    '5', 
-    '6', 
-    '7', 
-    '8', 
-    '9'
-  ],
-  lowerCasedCharacters: [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
-],
-
-  upperCasedCharacters:  [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'],
-}
-
 
 // Array of special characters to be included in password
 var specialCharacters = [
@@ -202,34 +106,38 @@ var upperCaseABC = true;
 var lowerCaseABC = true;
 var specialChar = true;
 var numericChar = true; 
+var password
 
 
 // Function to prompt user for password options
-/*function getPasswordOptions() {
-  passwordLength = prompt("Please input desired password length (8-128 characters)");
-  passwordCharacters = prompt("Please input desired characters (special, numeric, lower and upper.")
-}*/
 
 function getPasswordOptions(){
 passwordLength = prompt("Please input desired password length (8-128 characters)");
+  /*if ("8" > passwordLength < "128") { 
+    alert("Please select a number between 8 and 128")
+    getPasswordOptions()
+  }*/
 upperCaseABC = confirm("Would you like your password to include Upper Case characters?")
-lowerCaseABCCaseABC = confirm("Would you like your password to include Lower Case characters?")
+lowerCaseABC = confirm("Would you like your password to include Lower Case characters?")
 specialChar = confirm("Would you like your password to include Special Case characters?")
 numericChar = confirm("Would you like your password to include Numeric characters?")
 }
 
-
 // Function for getting a random element from an array
 function getRandom(arr) {
+    return arr[Math.floor((Math.random()*arr.length))];
+  }
+
+  console.log(getRandom())
   
-
-}
-
 // Function to generate password with user input
 function generatePassword() {
+  for (let i = 0; i < passwordLength; i++) {
+          password += getRandom(passwordCharacters[i])
+  }
 
-}
-
+};
+  
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
 
@@ -245,4 +153,64 @@ function writePassword() {
 generateBtn.addEventListener('click', writePassword);
 
 
+//UpperCase confirmed
+function UpperCaseCharlist() {
+  (passwordCharacters = upperCasedCharacters)
+      if (lowerCaseABC) {
+        passwordCharacters += lowerCasedCharacters
+      }
+      if (specialChar) {
+    passwordCharacters += specialCharacters
+}
+      if (numericChar) {
+    passwordCharacters += numericCharacters
+}
+}
+
+//lowerCase confirmed, not upperCase
+function LowerCaseCharlist() {
+  (passwordCharacters=lowerCasedCharacters)
+  if (specialChar) {
+    passwordCharacters += specialCharacters
+}
+      if (numericChar) {
+    passwordCharacters += numericCharacters
+}
+}
+//special confirmed, not upper or lower case
+function specialCharlist() {
+  (passwordCharacters = specialCharacters)
+if (numericChar) {
+  passwordCharacters += numericCharacters
+}
+}
+//numbers confirmed, not uppr,lower or special
+
+  //passwordCharacters = numericCharacters
+
+  //programme
+
 getPasswordOptions()
+
+
+if (upperCaseABC) {
+  UpperCaseCharlist()
+}
+else if (lowerCaseABC) {
+  LowerCaseCharlist()
+}
+else if (specialChar) {
+  specialCharlist()
+}
+else if (numericChar) {
+  passwordCharacters = numericCharacters
+}
+else {
+  alert("Please select at least one Character type")
+  getPasswordOptions()
+}
+
+generatePassword()
+  
+
+
